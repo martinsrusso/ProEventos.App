411db91b859629c64914648ac636cf0a4711fb21
Leonardo Nascimento <leonardo.nascimento@outlook.com>
2022-08-26 20:21:30 -0300

    Seção 4: Angular

--- Front/ProEventos-App/karma.conf.js
+++ Front/ProEventos-App/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ProEventos-App'),
+      dir: require('path').join(__dirname, './coverage/pro-eventos-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -3,22 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { AppComponent } from './app.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PalestrantesComponent,
     EventosComponent,
-    PalestrantesComponent
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
